bf5831cfcdc6a0a1a9ceea5d1495fbeafe16fd64
Michael-AngelCevallos <118484898+Michael-AngelCevallos@users.noreply.github.com>
2026-01-04 07:55:58 -0600

    Added Database

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Appointment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    location = db.Column(db.String(200))
+    location = db.Column(db.String(100), nullable=False)
     appointment_date = db.Column(db.DateTime, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
